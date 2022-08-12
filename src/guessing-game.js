f8ea0ffc3b19f5614d5a78cb1d78534a64ebe2b6
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2022-08-12 15:29:23 +0200

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middleNumber = Math.round((this.max + this.min) / 2);
+        return this.middleNumber;
     }
 
     lower() {
-
+        this.max = this.middleNumber;
     }
 
     greater() {
-
+        this.min = this.middleNumber;
     }
 }
 
